e2d099887678789ccbe2343c73799525d380cffa
Chase Strappello <CAStrappello@student.fullsail.edu>
2024-12-16 18:00:12 -0500

    Created looping switch menu with exit option and goodbye message

--- FlightAngleCalculator/FlightAngleCalculator.cpp
+++ FlightAngleCalculator/FlightAngleCalculator.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include "FlightAngleCalculator.h"
+#include <cstdlib>
 
 int FlightAngle(int a, int b)
 {
@@ -15,13 +16,53 @@ int FlightAngle(int a, int b)
 
 int main()
 {
-    std::cout << "~| Flight Angle Calculator |~" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Enter Horizontal Distance: ";
-    std::cin >> a;
-    std::cout << "Enter Vertical Distance: ";
-    std::cin >> b;
-    std::cout << std::endl;
+    int choice;
 
-    FlightAngle(a, b);
+    do
+    {
+        system("cls");
+        std::cout << "~| Flight Angle Calculator |~" << std::endl;
+        std::cout << std::endl;
+        std::cout << "1: Calculate Flight Angle\n";
+        std::cout << "2: Exit\n";
+        std::cout << std::endl;
+        std::cout << "Select A Menu Option: ";
+        std::cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                system("cls");
+                std::cout << std::endl;
+                std::cout << "Enter Horizontal Distance: ";
+                std::cin >> a;
+                std::cout << "Enter Vertical Distance: ";
+                std::cin >> b;
+                std::cout << std::endl;
+
+                FlightAngle(a, b);
+                system("pause");
+                break;
+
+            case 2:
+                system("cls");
+                std::cout << "Session Terminated!" << std::endl;
+                system("pause");
+                break;
+
+            default:
+                system("cls");
+                std::cout << "Invalid Selection!" << std::endl;
+                system("pause");
+        }
+        
+    }
+    while (choice != 2);
+
+    system("cls");
+    std::cout << "Thanks for using my app. See you nex time!" << std::endl;
+    system("pause");
+
+    system("cls");
+    return 2;
 }
